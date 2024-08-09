4e00f9d87354074414c376ff62d94cff585132a4
varawa <142249631+varawa@users.noreply.github.com>
2024-08-09 18:43:33 +0530

    variables

--- app.py
+++ app.py
@@ -4,3 +4,27 @@ print(" ||||")
 print("*" * 10)     #multiplying "*" string with 10 .
 
 #python code is interpreted line by line .
+
+#Variables
+#---------
+
+
+price = 10      #Integers
+#when 10 is about to be stored in a memory its first converted to its binary form.
+
+#reset price to 20 
+price = 20
+print("price : " , price)
+
+rating = 4.3    #float
+name = "mosh"   #string
+is_published = True #boolean
+
+#Python is CASE SENSITIVE. (False and not false) 
+
+
+#Task : We check in a patient named John Smith. He's 20 years old and is a new patient .
+
+name = "John Smith"
+age = 20 
+is_new = True
